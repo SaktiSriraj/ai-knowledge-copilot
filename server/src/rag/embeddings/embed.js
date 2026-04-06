f849fe67d43a2ac89c7bab8449cd48185c16ec39
Sakti Sriraj Mishra <saktisrirajmishra@gmail.com>
2026-04-06 19:21:07 +0530

    fix: implement batching for embedding requests to handle large documents and prevent API timeouts

--- server/src/rag/embeddings/embed.js
+++ server/src/rag/embeddings/embed.js
@@ -1,29 +1,39 @@
 import { openai } from "../../config/llm.js";
 
+const BATCH_SIZE = 10;
+
 export const embedText = async (texts) => {
- 
-  const formattedInput = texts.map((text) => ({
-    content: [
-      {
-        type: "text",
-        text,
-      },
-    ],
-  }));
 
-  const response = await openai.embeddings.create({
-    model: "nvidia/llama-nemotron-embed-vl-1b-v2:free",
-    input: formattedInput,
-    encoding_format: "float",
-  });
+    const allEmbeddings = [];
 
-  console.log("Embedding Response:", response);
+    for (let i = 0; i < texts.length; i += BATCH_SIZE) {
+        const batch = texts.slice(i, i + BATCH_SIZE);
+        
+        const formattedInput = batch.map((text) => ({
+            content: [{ type: "text", text }],
+        }));
 
-  if (!response.data) {
-    throw new Error(
-      `Embedding failed: ${response.error?.message || "Unknown error"}`
-    );
-  }
+        try {
+            const response = await openai.embeddings.create({
+                model: "nvidia/llama-nemotron-embed-vl-1b-v2:free",
+                input: formattedInput,
+                encoding_format: "float",
+            });
+            
+            console.log("Embedding Response:", response);
+            
+            if (!response.data) {
+                throw new Error(
+                    `Embedding failed: ${response.error?.message || "Unknown error"}`
+                );
+            }
 
-  return response.data.map((item) => item.embedding);
+            const batchEmbeddings = response.data.map((e) => e.embedding);
+
+            allEmbeddings.push(...batchEmbeddings);
+        } catch (error) {
+            console.error("Embedding Batch Failed", error.message);
+        }
+    }
+    return allEmbeddings;
 };
